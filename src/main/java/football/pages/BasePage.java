5a60a20be6cebd5e9b71dd472ee340859b6fd41c
Alejandro Ramirez <cloud91art@gmail.com>
2019-08-26 17:10:13 -0700

    Setting the base POM class with the general driver methods

---
+++ src/main/java/football/pages/BasePage.java
@@ -0,0 +1,74 @@
+package football.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
+import java.util.ArrayList;
+
+public class BasePage {
+    private static final int TIMEOUT = 5;
+    private static final int POLLING = 100;
+
+    protected WebDriver driver;
+    private WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver; }
+
+    //Base driver methods
+    public WebElement findElement(By locator){
+        return driver.findElement(locator);
+    }
+
+    public List<WebElement> findElements(By locator){
+        return driver.findElements(locator);
+    }
+
+    public String getText(WebElement element){
+        return element.getText();
+    }
+
+    public String getText(By locator){
+        return driver.findElement(locator).getText();
+    }
+
+    public void type(String inputText, By locator){
+        driver.findElement(locator).sendKeys(inputText);
+    }
+
+    public void click(By locator){
+        driver.findElement(locator).click();
+    }
+
+    public void click(WebElement element){
+        element.click();
+    }
+
+    public Boolean isDisplayed(By locator){
+        try {
+            return driver.findElement(locator).isDisplayed();
+        }catch (org.openqa.selenium.NoSuchElementException e){
+            return false;
+        }
+    }
+    protected void waitForElementToAppear(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected void waitForElementToDisappear(By locator) {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    protected void waitForTextToDisappear(By locator, String text) {
+        wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(locator, text)));
+    }
+
+    public void switchToTab(int tab){
+        ArrayList tabs = new ArrayList (driver.getWindowHandles());
+        //Use the list of window handles to switch between windows
+        driver.switchTo().window((String) tabs.get(tab -1));
+    }
+}

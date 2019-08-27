1625bf9f66f5baa58b1f9cc45cb09dd6524d312f
Alejandro Ramirez <cloud91art@gmail.com>
2019-08-26 17:12:34 -0700

    This page is for selecting whether classification or results

---
+++ src/main/java/football/pages/SantanderPage.java
@@ -0,0 +1,28 @@
+package football.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SantanderPage extends BasePage {
+    private By resultsLocator = By.xpath("//*[@id=\"__next\"]/div/header/div[2]/div/div[2]/div/div/div[2]/span/a");
+    private By clasifficationLocator = By.xpath("//*[@id=\"__next\"]/div/header/div[2]/div/div[2]/div/div/div[3]/span/a");
+    private By dropdownText = By.xpath("//*[@id=\"dropdownTitleId_211\"]/div/p");
+    private By resultsContainerLocator = By.cssSelector("div[class='styled__GameweekTable-qe32sx-3 fAhuR']");
+
+    public SantanderPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void goToResults(){
+
+        WebDriverWait ewait1 = new WebDriverWait(driver,10);
+        ewait1.until(ExpectedConditions.visibilityOfElementLocated(resultsLocator));
+
+        click(resultsLocator);
+
+        WebDriverWait ewait2 = new WebDriverWait(driver,10);
+        ewait2.until(ExpectedConditions.visibilityOfElementLocated(resultsContainerLocator));
+    }
+}

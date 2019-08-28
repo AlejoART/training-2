a105a660283b2b4792d9a7656c56731608827ce6
Alejandro Ramirez <cloud91art@gmail.com>
2019-08-27 17:20:10 -0700

    Added setLanguage() method for setting spanish if the page opens in english

--- src/test/java/training2/football/BaseTest.java
+++ src/test/java/training2/football/BaseTest.java
@@ -1,11 +1,14 @@
 package training2.football;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class BaseTest {
 
@@ -16,6 +19,7 @@ public class BaseTest {
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://www.laliga.com");
+        //setLanguage();
     }
 
     @AfterSuite
@@ -25,6 +29,16 @@ public class BaseTest {
 
     public WebDriver getDriver(){
         return driver;
+    }
+
+    public void setLanguage(){
+        List<WebElement> p = driver.findElements(By.cssSelector("styled__TextRegularContainer-sc-12cic8i-0 iqUYfN"));
+        By languageDropdown = By.id("dropdownTitleId_1622");
+        By spanish = By.cssSelector("li[class='styled__Item-sc-1yaayks-5 deobay']");
+        if(p.get(1).getText().equals("English")){
+            driver.findElement(languageDropdown).click();
+            driver.findElement(spanish).click();
+        }
     }
 
 }

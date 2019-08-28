1e84c885703cde5f068fa476b8b0a11854c5a8d4
Alejandro Ramirez <cloud91art@gmail.com>
2019-08-27 17:19:02 -0700

    Added checkMatchdayIsPlayed() and moveToPreviousMatchday() methods

--- src/main/java/football/pages/SantanderResultsPage.java
+++ src/main/java/football/pages/SantanderResultsPage.java
@@ -1,6 +1,5 @@
 package football.pages;
 
-import org.graalvm.compiler.core.phases.EconomyHighTier;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,8 +8,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SantanderResultsPage extends BasePage {
-    private By matchdaysDropdownLocator = By.id("dropdownTitleId_211");
 
+    //private List<WebElement> currentMatchdayFirstResult = findElements(By.cssSelector("p[class='styled__TextRegularContainer-sc-12cic8i-0 gHKnOS']"));
+    private By currentMatchdayFirstResult = By.xpath("//*[@id=\"accordion__heading-0\"]/div/div[3]/div/div/div[2]/div/p");
+    //private By matchdaysDropdownLocator = By.cssSelector("div[class='styled__DropDownContainer-sc-1yaayks-1 gECFwF']");
+    private List<WebElement> matchdaysDropdownLocator = findElements(By.cssSelector("div[class='styled__DropDownContainer-sc-1yaayks-1 gECFwF']"));
     //This one is for locating the results depending on the wanted team
     private By resultsListLocator = By.cssSelector("div[class='styled__ResultContainer-qe32sx-6 iYHKYB']");
     //This is the name of the team for comparing with the desired one
@@ -34,6 +36,26 @@ public class SantanderResultsPage extends BasePage {
         }
     }*/
 
+    //check if the current matchday has results or no, if no, move to the previous one
+    public void checkMatchdayIsPlayed(){
+        if(getText(currentMatchdayFirstResult).equals("VS")){
+        //Move to the previous matchday
+            moveToPreviousMatchday();
+        }
+        //DO the method for locating the X team and its score
+    }
+
+    public void moveToPreviousMatchday(){
+        click(matchdaysDropdownLocator.get(1));
+        String matchday = getText(matchdaysDropdownLocator.get(1).findElement(By.tagName("div")).findElement(By.tagName("p")));
+        String[] data = matchday.split(" ", 2);
+        int matchdayNumber = Integer.parseInt(data[1]);
+        List<WebElement> matchdaysList = matchdaysDropdownLocator.get(1).findElement(By.tagName("ul")).findElements(By.tagName("li"));
+        String lastMatchday = "Jornada 1";
+        while (!matchday.equals(lastMatchday)){
+            click(matchdaysList.get(matchdayNumber-2));
+        }
+    }
 
     public String getTeamLocator(String teamName){
         List<WebElement> namesDivList = findElements(teamNamesListLocator);

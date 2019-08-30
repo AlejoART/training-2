b94331ece8feb3cfb1ece0e05044a83e4b4714d5
Alejandro Ramirez <cloud91art@gmail.com>
2019-08-29 17:00:29 -0700

    Added the instantiation of the entities and the calculation of the score

--- src/test/java/training2/football/OsasunaResultsTest.java
+++ src/test/java/training2/football/OsasunaResultsTest.java
@@ -1,5 +1,7 @@
 package training2.football;
 
+import football.entities.Match;
+import football.entities.Team;
 import football.pages.HomePage;
 import football.pages.SantanderPage;
 import football.pages.SantanderResultsPage;
@@ -17,7 +19,21 @@ public class OsasunaResultsTest extends BaseTest {
         santanderPage.goToResults();
 
         SantanderResultsPage santanderResultsPage = new SantanderResultsPage(getDriver());
-        //santanderResultsPage.checkMatchdayIsPlayed();
-        santanderResultsPage.getTheCurrentMatchdayMatch("Osasuna");
+        String[] match = santanderResultsPage.getTheCurrentMatchdayMatch("Osasuna");
+
+        Match matchEntity = new Match(match[0], match[2], match[1]);
+        Team team = new Team();
+        matchEntity.setResultTeam1(match[2]);
+        int result1 = matchEntity.getResultTeam1();
+        matchEntity.setResultTeam1(match[2]);
+        int result2 = matchEntity.getResultTeam2();
+        if(matchEntity.getTeam1().contains("Osasuna".toUpperCase())){
+            team.setName(matchEntity.getTeam1());
+            team.setScore(matchEntity.addPointsToTeam1(result1, result2));
+        }else if (matchEntity.getTeam2().contains("Osasuna".toUpperCase())){
+            team.setName(matchEntity.getTeam2());
+            team.setScore(matchEntity.addPointsToTeam2(result1,result2));
+        }
+
     }
 }

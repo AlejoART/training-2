8f4183c2f83b948caa033f283051bd839620752b
Alejandro Ramirez <cloud91art@gmail.com>
2019-09-03 15:56:21 -0700

    Removed unused methods/atts and updated the existing ones

--- src/main/java/football/pages/SantanderResultsPage.java
+++ src/main/java/football/pages/SantanderResultsPage.java
@@ -5,14 +5,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.List;
 
 public class SantanderResultsPage extends BasePage {
 
-    private List<WebElement> matchdaysDropdownElement = findElements(By.cssSelector("div[class='styled__DropDownContainer-sc-1yaayks-1 gECFwF']"));
+    private List<WebElement> matchdaysDropdownElement;
     //This is the list for the 10 divs that contains the names of the teams and the result of the game in the tags p
-    private List<WebElement> matchesListElement = findElements(By.cssSelector("div[class='styled__ResultContainer-qe32sx-6 iYHKYB']"));
+    private List<WebElement> matchesListElement;
+    private By matchDiv = By.cssSelector("div[class='styled__GameweekTitle-qe32sx-2 jFqvgQ']");
     private By resultsContainerLocator = By.cssSelector("div[class='show styled__ShowLoadedContainer-sc-191gwr0-0 khRXzW']");
 
     public SantanderResultsPage(WebDriver driver) {
@@ -20,42 +20,39 @@ public class SantanderResultsPage extends BasePage {
     }
 
     public String[] getTheCurrentMatchdayMatch(String teamName){
+        WebDriverWait ewait1 = new WebDriverWait(driver,10);
+        ewait1.until(ExpectedConditions.visibilityOfElementLocated(resultsContainerLocator));
+
         matchesListElement = findElements(By.cssSelector("div[class='styled__ResultContainer-qe32sx-6 iYHKYB']"));
         String [] match = new String[3];
-        String team1 = null;
-        String team2 = null;
-        String result = null;
+
         for (int i = 0; i < matchesListElement.size(); i++) {
             List<WebElement> eachMatch = matchesListElement.get(i).findElements(By.tagName("p"));
             if(getText(eachMatch.get(0)).contains(teamName.toUpperCase()) && !getText(eachMatch.get(1)).equals("VS")){
-                team1 = getText(eachMatch.get(0));
-                result = getText(eachMatch.get(1));
-                team2 = getText(eachMatch.get(2));
-            }
-            else{
-                moveToPreviousMatchday(teamName);
+                match[0]  = getText(eachMatch.get(0));
+                match[1] = getText(eachMatch.get(1));
+                match[2] = getText(eachMatch.get(2));
+
                 break;
             }
             if (getText(eachMatch.get(2)).contains(teamName.toUpperCase()) && !getText(eachMatch.get(1)).equals("VS")) {
-                team1 = getText(eachMatch.get(0));
-                result = getText(eachMatch.get(1));
-                team2 = getText(eachMatch.get(2));
+                match[0] = getText(eachMatch.get(0));
+                match[1] = getText(eachMatch.get(1));
+                match[2] = getText(eachMatch.get(2));
+
+                break;
             }
-            else{
-                moveToPreviousMatchday(teamName);
+            if(getText(eachMatch.get(1)).equals("VS")){
                 break;
             }
         }
-        match[0] = team1;
-        match[1] = result;
-        match[2] = team2;
-
         return match;
     }
 
-    public void moveToPreviousMatchday(String teamName){
+    public void moveToPreviousMatchday(){
+        matchdaysDropdownElement = findElements(By.cssSelector("div[class='styled__DropDownContainer-sc-1yaayks-1 gECFwF']"));
         click(matchdaysDropdownElement.get(1));
-        String matchday = getText(matchdaysDropdownElement.get(1).findElement(By.tagName("div")).findElement(By.tagName("p")));
+        String matchday = getText(matchdaysDropdownElement.get(1).findElement(By.tagName("p")));
         String[] data = matchday.split(" ", 2);
         int matchdayNumber = Integer.parseInt(data[1]);
         List<WebElement> matchdaysList = matchdaysDropdownElement.get(1).findElement(By.tagName("ul")).findElements(By.tagName("li"));
@@ -65,16 +62,18 @@ public class SantanderResultsPage extends BasePage {
 
             WebDriverWait ewait1 = new WebDriverWait(driver,10);
             ewait1.until(ExpectedConditions.visibilityOfElementLocated(resultsContainerLocator));
-
-            getTheCurrentMatchdayMatch(teamName);
         }
-
     }
 
+    public int getMatchdaysAmount(){
+        WebDriverWait ewait1 = new WebDriverWait(driver,10);
+        ewait1.until(ExpectedConditions.visibilityOfElementLocated(matchDiv));
 
-    //In this method will be saved the points achieved by team X, first in the current matchday, then in the previous matchdays
-    //by selecting the played game of the X team and inside that div select the tag p where is displayed the score 0 - 0
-    public int getPointsTeamXperMtachday(By teamLocator){
-        return 0;
+        matchdaysDropdownElement = findElements(By.cssSelector("div[class='styled__DropDownContainer-sc-1yaayks-1 gECFwF']"));
+
+        String matchday = getText(matchdaysDropdownElement.get(1));
+        String[] data = matchday.split(" ", 2);
+        int matchdayNumber = Integer.parseInt(data[1]);
+        return matchdayNumber;
     }
 }

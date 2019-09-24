e11e1a4a7492ca8ba8eed7511a46e4f39cd327e7
Alejandro Ramirez <cloud91art@gmail.com>
2019-09-24 10:10:49 -0700

    Updated getTheCurrentMatchdayMatch() method

--- src/main/java/football/pages/SantanderResultsPage.java
+++ src/main/java/football/pages/SantanderResultsPage.java
@@ -24,26 +24,18 @@ public class SantanderResultsPage extends BasePage {
         ewait1.until(ExpectedConditions.visibilityOfElementLocated(resultsContainerLocator));
 
         matchesListElement = findElements(By.cssSelector("div[class='styled__ResultContainer-qe32sx-6 iYHKYB']"));
-        String [] match = new String[3];
+        String [] match = new String[4];
 
         for (int i = 0; i < matchesListElement.size(); i++) {
             List<WebElement> eachMatch = matchesListElement.get(i).findElements(By.tagName("p"));
-            if(getText(eachMatch.get(0)).contains(teamName.toUpperCase()) && !getText(eachMatch.get(1)).equals("VS")){
-                match[0]  = getText(eachMatch.get(0));
-                match[1] = getText(eachMatch.get(1));
-                match[2] = getText(eachMatch.get(2));
-
-                break;
-            }
-            if (getText(eachMatch.get(2)).contains(teamName.toUpperCase()) && !getText(eachMatch.get(1)).equals("VS")) {
-                match[0] = getText(eachMatch.get(0));
-                match[1] = getText(eachMatch.get(1));
-                match[2] = getText(eachMatch.get(2));
-
-                break;
-            }
-            if(getText(eachMatch.get(1)).equals("VS")){
-                break;
+            if (!getText(eachMatch.get(1)).equals("VS")) {
+                if (getText(eachMatch.get(0)).contains(teamName.toUpperCase()) || getText(eachMatch.get(4)).contains(teamName.toUpperCase())) {
+                    match[0] = getText(eachMatch.get(0));
+                    match[1] = getText(eachMatch.get(4));
+                    match[2] = getText(eachMatch.get(1));
+                    match[3] = getText(eachMatch.get(3));
+                    break;
+                }
             }
         }
         return match;
@@ -56,12 +48,17 @@ public class SantanderResultsPage extends BasePage {
         String[] data = matchday.split(" ", 2);
         int matchdayNumber = Integer.parseInt(data[1]);
         List<WebElement> matchdaysList = matchdaysDropdownElement.get(1).findElement(By.tagName("ul")).findElements(By.tagName("li"));
-        String lastMatchday = "Jornada 1";
-        if (!matchday.equals(lastMatchday)){
+
+        if (matchdayNumber!= 1){
+            WebDriverWait ewait = new WebDriverWait(driver,10);
+            ewait.until(ExpectedConditions.elementToBeClickable(matchdaysList.get(matchdayNumber-2)));
+
             click(matchdaysList.get(matchdayNumber-2));
 
             WebDriverWait ewait1 = new WebDriverWait(driver,10);
             ewait1.until(ExpectedConditions.visibilityOfElementLocated(resultsContainerLocator));
+
+            switchToTab(1);
         }
     }
 
@@ -73,7 +70,6 @@ public class SantanderResultsPage extends BasePage {
 
         String matchday = getText(matchdaysDropdownElement.get(1));
         String[] data = matchday.split(" ", 2);
-        int matchdayNumber = Integer.parseInt(data[1]);
-        return matchdayNumber;
+        return Integer.parseInt(data[1]);
     }
 }
